cc6bc42dda1df29771aeede655c648fdf6a287f4
Christopher Kolstad <christopher.kolstad@gmail.com>
2015-05-24 04:51:46 +0200

    Added column test

--- test/sudoku/BoardTest.scala
+++ test/sudoku/BoardTest.scala
@@ -1,10 +1,9 @@
 package sudoku
 
-import org.specs2.Specification
 import org.specs2.matcher.ShouldMatchers
 import play.api.test.PlaySpecification
 
-object BoardTest extends PlaySpecification with ShouldMatchers {
+object RowTest extends PlaySpecification {
   val emptyRow = Row.empty
   val partialRow = Row(Some(Eight), None, None, Some(Five), Some(Four), None, None, Some(One), Some(Nine))
   val fullRow = Row(Some(Eight), Some(Seven), Some(Six), Some(Five), Some(Four), Some(Three), Some(Two), Some(One), Some(Nine))
@@ -41,4 +40,47 @@ object BoardTest extends PlaySpecification with ShouldMatchers {
     }
   }
 
+}
+
+object ColumnTest extends PlaySpecification {
+  val emptyColumn = Column(None, None, None, None, None, None, None, None, None)
+  val partialColumn = Column(Some(Eight), None, None, Some(Five), Some(Four), None, None, Some(One), Some(Nine))
+  val fullColumn = Column(Some(Eight), Some(Seven), Some(Six), Some(Five), Some(Four), Some(Three), Some(Two), Some(One), Some(Nine))
+  val invalidColumn = partialColumn.copy(sixth = Some(Five))
+
+  "Column" should {
+    "deindex correctly for empty column" in {
+      SudokuNumber.all map {
+        emptyColumn(_)
+      } must equalTo(
+        List(None, None, None, None, None, None, None, None, None))
+    }
+    "deindex correctly for partial column" in {
+      SudokuNumber.all map {
+        partialColumn(_)
+      } must equalTo(
+        List(Some(Eight), None, None, Some(Five), Some(Four), None, None, Some(One), Some(Nine)))
+    }
+
+    "deindex correctly for full column" in {
+      SudokuNumber.all map {
+        fullColumn(_)
+      } must equalTo(
+        List(Some(Eight), Some(Seven), Some(Six), Some(Five), Some(Four), Some(Three), Some(Two), Some(One), Some(Nine)))
+    }
+
+    "deindex correctly for invalid column" in {
+      SudokuNumber.all map {
+        invalidColumn(_)
+      } must equalTo(
+        List(Some(Eight), None, None, Some(Five), Some(Four), Some(Five), None, Some(One), Some(Nine)))
+    }
+
+    "have equality test based on immutability" in {
+      emptyColumn must equalTo(Column(None, None, None, None, None, None, None, None, None))
+      partialColumn must equalTo(Column(Some(Eight), None, None, Some(Five), Some(Four), None, None, Some(One), Some(Nine)))
+      fullColumn must equalTo(Column(Some(Eight), Some(Seven), Some(Six), Some(Five), Some(Four), Some(Three), Some(Two), Some(One), Some(Nine)))
+      invalidColumn must equalTo(Column(Some(Eight), None, None, Some(Five), Some(Four), Some(Five), None, Some(One), Some(Nine)))
+    }
+  }
 }
